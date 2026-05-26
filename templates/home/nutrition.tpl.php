9e723738bde86cc9ffb6e7241aefccd9ebab8a45
Narciso Terrão Barroso <tbnarciso13@gmail.com>
2026-05-26 01:08:51 +0100

    Small change in nutrion section

--- templates/home/nutrition.tpl.php
+++ templates/home/nutrition.tpl.php
@@ -37,7 +37,7 @@
                     <div class="nutrition-card">
                         <img src="../img/omega3.jpg" alt="Omega-3 Delight" class="nutrition-image">
                         <span class="nutrition-badge">350 kcal</span>
-                        <h3 class="nutrition-name">🐟 Omega-3 Delight</h3>
+                        <h3 class="nutrition-name">Omega-3 Delight</h3>
                         <p class="nutrition-description">Salmon, wild rice, asparagus. Heart-healthy and muscle-supporting.</p>
                     </div>
                 </div>
@@ -46,7 +46,7 @@
                     <div class="nutrition-card">
                         <img src="../img/breakfast.png" alt="Morning Fuel" class="nutrition-image">
                         <span class="nutrition-badge">400 kcal</span>
-                        <h3 class="nutrition-name">🥚 Morning Fuel</h3>
+                        <h3 class="nutrition-name">Morning Fuel</h3>
                         <p class="nutrition-description">Egg white omelet with mushrooms, oatmeal, berries. Energy for your day.</p>
                     </div>
                 </div>
@@ -55,7 +55,7 @@
                     <div class="nutrition-card">
                         <img src="../img/protein smoothie.jpg" alt="Recovery Smoothie" class="nutrition-image">
                         <span class="nutrition-badge">300 kcal</span>
-                        <h3 class="nutrition-name">🍓 Recovery Smoothie</h3>
+                        <h3 class="nutrition-name">Recovery Smoothie</h3>
                         <p class="nutrition-description">Protein powder, banana, berries, Greek yogurt. Quick post-workout recovery.</p>
                     </div>
                 </div>
